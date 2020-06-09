d30fa7f03f6362d1d93633adbc48d2ee1372082f
Sean Murray <sean.j.murray1@gmail.com>
2020-06-09 15:39:33 -0700

    Add superagent, change to API calls, add /trails

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # city-explorer
 **Author**: Sean Murray
-**Version**: 1.0.0 
+**Version**: 1.5.0 
 
 ## Overview
 This will be a simple API to serve information for the City Explorer Front end.
@@ -34,13 +34,14 @@ This API is designed to handle GET requests on /location and /weather routes to 
  - Express
  - dotenv
  - cors
+ - superagent
 
 ## Change Log
  - 06-08-2020 1531 - Application now has functional GET route for location.
 
  - 06-08-2020 1615 Application now has function Get route for weather.
 
-
+ - 06-09-2020 1530 Application now uses API calls to query search data, and GET route for trails.
 
 ## Credits and Collaborations
 
@@ -56,6 +57,8 @@ This API is designed to handle GET requests on /location and /weather routes to 
 
 **Actual time needed to complete:** 30min
 
+
+
 **Number and name of feature:** Weather
 
 **Estimate of time needed to complete:** 30min
@@ -65,3 +68,35 @@ This API is designed to handle GET requests on /location and /weather routes to 
 **Finish time:** 1600
 
 **Actual time needed to complete:** 15min
+
+
+**Number and name of feature:** Trails
+
+**Estimate of time needed to complete:** 30min
+
+**Start time:** 1430
+
+**Finish time:** 1500
+
+**Actual time needed to complete:** 30min
+
+
+**Number and name of feature:** Superagent + API
+
+**Estimate of time needed to complete:** 30min
+
+**Start time:** 1445
+
+**Finish time:** 1515
+
+**Actual time needed to complete:** 30min
+
+## Whiteboards for logic:
+
+### Day 1
+![Day 1](whiteboards/Day1.png)
+
+
+### Day 2
+![Day 2](whiteboards/Day2.png)
+
--- server.js
+++ server.js
@@ -3,15 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const superagent = require('superagent');
 app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 /////////////////////////location route////////////////////////////////
 app.get('/location',(req,res)=>{
   try{
-    let apiData = require('./data/location.json');
-    let retObj = new Location(req.query.city,apiData[0]);
-    res.status(200).send(retObj);
+    let search = req.query.city
+    let url = `${process.env.LOC_API}?key=${process.env.LOC_KEY}&q=${search}&format=json`
+    superagent.get(url)
+      .then(apiData =>{
+        let retObj = new Location(search,apiData.body[0]);
+        res.status(200).send(retObj);
+      }).catch(err => console.log(err))
   }catch(err){
     res.status(500).send('Sorry, something went wrong');
   }
@@ -26,9 +31,13 @@ function Location(search,obj){
 //////////////////////////Weather route////////////////////////////////
 app.get('/weather', (req,res)=>{
   try{
-    let apiData = require('./data/weather.json');
-    let retArr = apiData.data.map((obj)=> new Weather(obj))
-    res.status(200).send(retArr);
+    let loc = req.query.search_query;
+    let url = `${process.env.WEA_API}?city=${loc}&units=i&key=${process.env.WEA_KEY}`;
+    superagent.get(url)
+      .then(apiData => {
+        let retArr = apiData.body.data.map((obj)=> new Weather(obj))
+        res.status(200).send(retArr);
+      }).catch(err => console.log(err))
   }catch(err){
     res.status(500).send('Sorry, something went wrong');
   }
@@ -38,6 +47,33 @@ app.get('/weather', (req,res)=>{
 function Weather(obj){
   this.forecast=obj.weather.description;
   this.time=obj.valid_date;
+}
+/////////////////////////////Trail route///////////////////////////////
+app.get('/trails', (req,res) => {
+  try{
+    let loc = [req.query.latitude,req.query.longitude];
+    let url = `${process.env.TRAIL_API}?lat=${loc[0]}&lon=${loc[1]}&maxDistance=10&key=${process.env.TRAIL_KEY}`
+    superagent.get(url)
+      .then(apiData => {
+        let retArr=apiData.body.trails.map(obj => new Trail(obj));
+        res.status(200).send(retArr);
+      }).catch(err => console.log(err))
+  }catch(err){
+    res.status(500).send('Sorry, something went wrong');
+  }
+})
+////////////////////////////TRAIL CONSTRUCTOR//////////////////////////
+function Trail(obj){
+  this.name=obj.name;
+  this.location=obj.location;
+  this.length=obj.length;
+  this.stars=obj.stars;
+  this.star_votes=obj.starVotes;
+  this.summary=obj.summary;
+  this.trail_url=obj.url;
+  this.conditions=`${obj.conditionDetails || ''} ${obj.conditionStatus}`;
+  this.conditions_date=obj.conditionDate.slice(0,obj.conditionDate.indexOf(' '));
+  this.conditions_time=obj.conditionDate.slice(obj.conditionDate.indexOf(' ')+1,obj.conditionDate.length);
 }
 
 ////////////////////////////All other routes///////////////////////////
